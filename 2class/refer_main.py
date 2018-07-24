7e061396b724a55f280b0a7d7e4fdb96888dc189
YumaFujioka <tpddfs@live.jp>
2018-07-24 17:53:41 +0900

    メイン関数のテンプレートっぽいのを作成

---
+++ 2class/refer_main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# --------------------
+# **パッケージ管理**
+# --------------------
+from os import path
+import numpy as np
+import csv
+import glob
+
+# --------------------
+# **ファイル管理**
+# --------------------
+# このプログラムがあるディレクトリ絶対パスを取得
+script_dir = path.abspath(path.dirname(__file__))
+
+csv_files = glob.glob( path.join(script_dir, 'data_gen_20180724/*.csv') )
+
+# --------------------
+# **関数部**
+# --------------------
+def MakeListCSVasXY(List):
+    return [ [ np.array(  x[:-1] ), x[-1] ] for x in List]
+
+# --------------------
+# **メイン部**
+# --------------------
+if __name__ == '__main__':
+
+    # --------------------
+    # **ロード部**
+    # --------------------
+    dataset = []
+    for file in csv_files:
+        with open(file) as f:
+            reader = csv.reader(f)
+            dataset.append( [[float(x[0]), float(x[1]), int(x[2])] for x in reader] )
+
+    print( MakeListCSVasXY(dataset[0]) )
